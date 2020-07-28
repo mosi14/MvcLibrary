1d2a84b41f51a67b4b222026e10f559764368c1f
mosi <m.asadollahy@hotmail.com>
2020-07-29 00:16:09 +0430

    create home page

--- E-ShopBook/E-ShopBook/Areas/Customer/Controllers/HomeController.cs
+++ E-ShopBook/E-ShopBook/Areas/Customer/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using E_ShopBook.DataAccess.Repository.IRepository;
+using E_ShopBook.Models;
 using E_ShopBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -13,16 +15,19 @@ namespace E_ShopBook.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
