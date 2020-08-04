45d64b7821a40a67e48e8cd4b3a8dcc74c73b387
mosi <m.asadollahy@hotmail.com>
2020-08-04 21:49:06 +0430

    Shopping Cart Management

--- E-ShopBook/E-ShopBook/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ E-ShopBook/E-ShopBook/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -167,7 +167,7 @@ namespace E_ShopBook.Areas.Identity.Pages.Account
                         "/Account/ConfirmEmail",
                         pageHandler: null,
                         values: new { area = "Identity", userId = user.Id, code = code },
-                        protocol: Request.Scheme);        
+                        protocol: Request.Scheme);
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
@@ -195,6 +195,19 @@ namespace E_ShopBook.Areas.Identity.Pages.Account
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
+            Input = new InputModel()
+            {
+                CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                RoleList = _roleManager.Roles.Where(u => u.Name != SD.Role_User_Indi).Select(x => x.Name).Select(i => new SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
 
             // If we got this far, something failed, redisplay form
             return Page();
